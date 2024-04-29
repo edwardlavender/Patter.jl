e949523db5119efc09768f6870e568e11c044c3a
edwardlavender <edward.lavender@eawag.ch>
2024-04-29 10:37:13 +0200

    Add documentation

--- src/001-spatial.jl
+++ src/001-spatial.jl
@@ -2,10 +2,22 @@ import GeoArrays
 
 export extract
 
-# Extract values from an environmental layer
-# * The default method assumes env is a GeoArray
-# * A custom method is required to handle other types (e.g., shapefiles)
+"""
+    extract(env::env::GeoArrays.GeoArray, x::Real, y::Real)
 
+Extract the value of a `GeoArray` (such as a bathymetry grid) at a specififed pair of `x` and `y` coordinates.
+
+# Details
+
+In `Patter.jl`, `NaN` elements are taken to define inhospitable habitats, such as land (see `is_valid()`). 
+
+To extract a value from an environmental layer in another format, such as a shapefile, write a custom `extract()` method.
+
+# Returns
+
+- The value of the `GeoArray` for a coordinate pair within the bounds of `env`;
+- NaN (of the same type as `env`'s elements) for a coordinate pair beyond the bounds of `env`;
+"""
 function extract(env::GeoArrays.GeoArray, x::Real, y::Real)
     cell = GeoArrays.indices(env, (x, y))
     if checkbounds(Bool, env, cell)
@@ -15,7 +27,28 @@ function extract(env::GeoArrays.GeoArray, x::Real, y::Real)
     end
 end
 
-# Determine if a location is valid (i.e., in water)
+
+"""
+    is_valid(env, x::Real, y::Real)
+    is_valid(env, x::Real, y::Real, z::Real)
+
+Determine the validity of a point within a given environment.
+
+For the 2D case (`is_valid(env, x, y)`), this function checks if the z-coordinate obtained from `env` for the point (x, y) is not `NaN`.
+
+For the 3D case (`is_valid(env, x, y, z)`), it checks if the z-coordinate obtained for the point (x, y) is not `NaN`, and also ensures that the provided z-coordinate `z` lies within the valid range, specifically 0 < z ≤ z_env.
+
+# Arguments
+- `env`: The environment from which z-coordinates are extracted;
+- `x`: The x-coordinate of the point;
+- `y`: The y-coordinate of the point;
+- `z`: The z-coordinate of the point to be validated (only for the 3D case);
+
+# Returns
+- `true` if the conditions for validity are met;
+- `false` otherwise;
+""" 
+function is_valid end
 
 # 2D case
 function is_valid(env, x::Real, y::Real)
@@ -27,7 +60,7 @@ function is_valid(env, x::Real, y::Real)
     end 
 end
 
-# 3D case 
+# 3D case
 function is_valid(env, x::Real, y::Real, z::Real)
     z_env = extract(env, x, y)
     if isnan(z_env) 
@@ -37,9 +70,23 @@ function is_valid(env, x::Real, y::Real, z::Real)
     else 
         false
     end 
-end
+end 
 
-# Distances
+
+"""
+    distance(x0::Real, y0::Real, x1::Real, y1::Real)
+
+Calculate Euclidean distances between coordinates. 
+
+# Arguments
+- `x0`, `y0`: The coordinates of the first point;
+- `x1`, `y1`: The coordinates of the second point;
+
+# Returns
+- A number that defines the distance between two coordinates.
+
+"""
+function distance end
 
 # 2D case
 function distance(x0::Real, y0::Real, x1::Real, y1::Real)
