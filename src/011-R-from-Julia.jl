8350f88b6846170d652488b69f596c23d73d56e8
edwardlavender <edward.lavender@eawag.ch>
2024-05-03 17:03:25 +0200

    Add r_get_dataset() function

--- src/011-R-from-Julia.jl
+++ src/011-R-from-Julia.jl
@@ -7,6 +7,7 @@ A collection of functions that facilitate the translation of inputs from `Julia`
 
 # Details
 * [`r_get_states`] translates a State matrix into a `DataFrame` that can be passed to `R`. In the input matrix, each row is a particle and each column is a time step. 
+* [`r_get_dataset`] translates a Dictionary of observations into a Vector of DataFrames that can be passed to `R`.
 
 # Returns 
 A long-format `DataFrame`, with columns for `path_id`, `timestep` and each state dimension.
@@ -45,3 +46,52 @@ end
 # nt = 20000
 # state = [StateXY(rand(), rand(), rand()) for _ in 1:np, _ in 1:nt]
 # r_get_states(state)
+
+
+#### Formulate dictionaries to hold the observation for a selected time stamp
+# Generic function (for user-specified types)
+function dict_obs(timestamp, obs, sensor, model_type::Type{<: ModelObs})
+    Dict("timestamp" => timestamp, "observation" >= obs)
+end 
+# Specialised function for depth observations
+function dict_obs(timestamp, obs, sensor, model_type::Type{ModelObsAcousticLogisTrunc})
+    if obs == 1
+        Dict("timestamp" => timestamp, "receiver_x" => sensor.x, "receiver_y" => sensor.y, "detection" => obs)
+    else
+        Dict() 
+    end 
+end 
+# Specialised function for acoustic observations 
+function dict_obs(timestamp, obs, sensor, model_type::Union{Type{ModelObsDepthUniform}, Type{ModelObsDepthNormalTrunc}})
+    Dict("timestamp" => timestamp, "depth" => obs)
+end 
+
+#### Extract dataset(s) from yobs for a selected model type
+function r_get_dataset(yobs::Dict, model_type::Type{<: ModelObs})
+
+    # Initialise a Vector of DataFrames 
+    # * One dataframe for each dictionary element (i.e., simulated path)
+    # * Note that output = [] is required for correct translation via JuliaCall 
+    output = [] # Vector{DataFrame}()
+
+    # Build the vector of dataframes
+    for key in sort(collect(keys(yobs)))
+        # Initialise a Vector of dictionaries
+        # * Each dictionary will contain, for a specified time stamp, the observation & essential model parameters 
+        dicts = Vector{Dict}()
+        for (timestamp, observations) in yobs[key]
+            for (obs, sensor) in observations
+                if sensor isa model_type
+                    push!(dicts, dict_obs(timestamp, obs, sensor, model_type))
+                end
+            end 
+        end
+        # Convert the dictionaries to a DataFrame
+        df = DataFrame(dicts)
+        df.key .= key 
+        push!(output, df)
+    end 
+
+    output
+
+end 
