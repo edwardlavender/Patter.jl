dd41273f1e326f29920a1b99ecd7f3e09da55274
edwardlavender <edward.lavender@eawag.ch>
2024-05-03 15:01:40 +0200

    Update julia_get_*() functions

--- src/010-Julia-from-R.jl
+++ src/010-Julia-from-R.jl
@@ -1,6 +1,5 @@
 using DataFrames
-
-
+using Tables
 
 """
 # Julia from R
@@ -10,10 +9,13 @@ A collection of functions that facilitate the translation of inputs from R into 
 # Details
 
 * [`julia_get_xinit()`] gets a Vector of initial States from a DataFrame;
-* [`julia_get_models()`] gets a Vector of `ModelObs` subtypes from a Vector of Strings;
+* [`julia_get_model_types()`] gets a Vector of `ModelObs` subtypes from a Vector of Strings;
+* [`julia_get_models()`] gets a Vector of model instances from a Vector of DataFrames that contain parameters and a corresponding vector of model types;
 
 """
 function julia_get end 
+
+#### Get the Vector of initial States 
 
 function julia_get_xinit(state::Type{StateXY}, d::DataFrame)
     [StateXY(d.map_value[i], d.x[i], d.y[i]) for i in 1:nrow(d)]
@@ -31,10 +33,25 @@ end
 # d = DataFrame(map_value = [0, 1], x = [1, 2], y = [3, 4])
 # julia_get_xinit(StateXY, d)
 
+#### Get a vector of ModelObs types 
 
-function julia_get_models(models::Vector{String})
+function julia_get_model_types(models::Vector{String})
     [eval(Symbol(model)) for model in models]
 end
 
 # julia_get_models(["ModelObsAcousticLogisTrunc"])
 # julia_get_models(["ModelObsAcousticLogisTrunc", "ModelObsDepthUniform"])
+
+dataset = DataFrame(x = 1.1, y = 1.1, alpha = 1.1, beta = 1.1, gamma = 1.1)
+model_type = ModelObsAcousticLogisTrunc
+
+#### Get a vector of ModelObs instances
+function julia_get_models(parameters::Vector, model_types::Vector{DataType})
+    sensors = Vector{ModelObs}()
+    for (dataset, model_type) in zip(parameters, model_types)
+        for row in Tables.namedtupleiterator(dataset)
+            push!(sensors, model_type(row...))
+        end 
+    end 
+    sensors
+end 
