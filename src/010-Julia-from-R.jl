5267c3985c2841487c4526b19f865732e3c73dee
edwardlavender <edward.lavender@eawag.ch>
2024-05-27 13:19:34 +0200

    Use model_obs

--- src/010-Julia-from-R.jl
+++ src/010-Julia-from-R.jl
@@ -9,8 +9,8 @@ A collection of internal functions that facilitate the translation of objects fr
 # Details
 
 * [`Patter.julia_get_xinit()`](@ref) gets a `Vector` of initial [`State`](@ref)s from a `DataFrame`;
-* [`Patter.julia_get_model_types()`](@ref) gets a `Vector` of [`ModelObs`](@ref) sub-types from a Vector of `String`s;
-* [`Patter.julia_get_models()`](@ref) gets a `Vector` of [`ModelObs`](@ref) instances from a `Vector` of `DataFrame`s that contain parameters and a corresponding vector of [`ModelObs`](@ref) sub-types;
+* [`Patter.julia_get_model_obs_types()`](@ref) gets a `Vector` of [`ModelObs`](@ref) sub-types from a Vector of `String`s;
+* [`Patter.julia_get_model_obs()`](@ref) gets a `Vector` of [`ModelObs`](@ref) instances from a `Vector` of `DataFrame`s that contain parameters and a corresponding vector of [`ModelObs`](@ref) sub-types;
 
 """
 function julia_get end 
@@ -27,17 +27,17 @@ end
 
 #### Get a vector of ModelObs types 
 
-function julia_get_model_types(models::Union{String, Vector{String}})
+function julia_get_model_obs_types(models::Union{String, Vector{String}})
     models = isa(models, String) ? [models] : models
     return [getfield(Main, Symbol(model)) for model in models]
 end
-@doc (@doc julia_get) julia_get_model_types
+@doc (@doc julia_get) julia_get_model_obs_types
 
-# julia_get_models(["ModelObsAcousticLogisTrunc"])
-# julia_get_models(["ModelObsAcousticLogisTrunc", "ModelObsDepthUniform"])
+# julia_get_model_obs_types(["ModelObsAcousticLogisTrunc"])
+# julia_get_model_obs_types(["ModelObsAcousticLogisTrunc", "ModelObsDepthUniform"])
 
 #### Get a vector of ModelObs instances
-function julia_get_models(parameters::Vector, model_types::Vector{DataType})
+function julia_get_model_obs(parameters::Vector, model_types::Vector{DataType})
     sensors = Vector{ModelObs}()
     for (dataset, model_type) in zip(parameters, model_types)
         # TO DO
@@ -48,4 +48,4 @@ function julia_get_models(parameters::Vector, model_types::Vector{DataType})
     end 
     sensors
 end 
-@doc (@doc julia_get) julia_get_models
+@doc (@doc julia_get) julia_get_model_obs
